a69bd1b1bc4daea8e2a4a935c66995299c2d9174
felipe29j <55294238+felipe29j@users.noreply.github.com>
2024-07-21 13:18:12 -0300

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,21 +10,33 @@ class SearchController extends Controller
     public function search($ceps)
     {
         $cepArray = explode(',', $ceps);
-    
-        $enderecos = [];
-    
+        $result = [];
+
         foreach ($cepArray as $cep) {
+            $cep = str_replace('-', '', $cep);
             $response = Http::withOptions([
                 'verify' => false, // Desativa a verificação SSL
             ])->get("https://viacep.com.br/ws/{$cep}/json/");
-    
-            if ($response->successful()) {
-                $enderecos[] = $response->json();
+            
+            $data = $response->json();
+
+            if (!isset($data['erro'])) {
+                $result[] = [
+                    'cep' => $data['cep'] ? str_replace('-', '', $data['cep']) : '',
+                    'label' => "{$data['logradouro']}, {$data['localidade']}",
+                    'logradouro' => $data['logradouro'] ?? '',
+                    'complemento' => $data['complemento'] ?? '',
+                    'bairro' => $data['bairro'] ?? '',
+                    'localidade' => $data['localidade'] ?? '',
+                    'uf' => $data['uf'] ?? '',
+                    'ibge' => $data['ibge'] ?? '',
+                    'gia' => $data['gia'] ?? '',
+                    'ddd' => $data['ddd'] ?? '',
+                    'siafi' => $data['siafi'] ?? ''
+                ];
             }
         }
-    
-        return response()->json($enderecos);
-    }
-    
-}
 
+        return response()->json($result);
+    }
+}
